3e786c2ae343cfba5b5d942f49a4d84035c71883
newmember85 <46863511+newmember85@users.noreply.github.com>
2021-01-09 13:08:40 +0100

    Update startup.java

--- TestProject/src/PSTA/startup.java
+++ TestProject/src/PSTA/startup.java
@@ -38,7 +38,7 @@ class startup {
         grafikArt.find("Apple");
         
         System.out.println("hallo Welt hahah");
-        System.out.println("hallo welt");
+        System.out.println("hallo welt du lappen");
         
 
 
